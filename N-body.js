54d70d8df63e55de1cb0f5c01221b5a6e7218eda
archita-b <archita.bhattacharyya91@gmail.com>
2024-11-25 13:33:17 +0530

    fix bug in N-body.js

--- N-body.js
+++ N-body.js
@@ -21,15 +21,20 @@ function computeAcceleration() {
   for (let i = 0; i < n; i++) {
     particles[i].ax = 0;
     particles[i].ay = 0;
+  }
 
-    for (let j = i + 1; j < n - 1; j++) {
+  for (let i = 0; i < n; i++) {
+    for (let j = i + 1; j < n; j++) {
       const dx = particles[j].x - particles[i].x;
       const dy = particles[j].y - particles[i].y;
-      let dr = Math.sqrt(dx ** 2 + dy ** 2);
-
-      if (dr < minDistance) continue;
+      const dr = Math.sqrt(dx ** 2 + dy ** 2);
 
       const F = (G * mass * mass) / dr ** 2;
+
+      if (dr < minDistance) {
+        continue;
+      }
+
       const ax = (F / mass) * (dx / dr);
       const ay = (F / mass) * (dy / dr);
 
